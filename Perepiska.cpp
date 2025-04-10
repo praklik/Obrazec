2cad4d122a7fe8a62cebe7b7d74fca19b5947255
praklik <kowkapoff@gmail.com>
2025-04-10 21:52:25 +0300

    Add struct Employee

--- Perepiska.cpp
+++ Perepiska.cpp
@@ -7,6 +7,19 @@
 #include <iomanip>
 using namespace std;
 
+// Структура для хранения данных о сотруднике
+struct Employee {
+    string name;        // Имя сотрудника
+    int age;            // Возраст сотрудника
+
+    Employee* next;     // Указатель на следующего сотрудника
+    Employee* prev;     // Указатель на предыдущего сотрудника
+};
+
+// Глобальные переменные
+Employee* head = NULL;  // Указатель на начало списка
+Employee* tail = NULL;  // Указатель на конец списка
+
 int main() {
     setlocale(LC_ALL, "Russian");
     system("color 2");
