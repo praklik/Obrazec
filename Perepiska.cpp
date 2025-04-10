988fef2817681082dca253c8e08595f3b775adde
praklik <kowkapoff@gmail.com>
2025-04-10 21:28:48 +0300

    First (Initial) commit.

---
+++ Perepiska.cpp
@@ -0,0 +1,13 @@
+#include <conio.h>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
